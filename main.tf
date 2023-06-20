16f7ff54deea4e58b21ce2089e1fedf597024401
Sam <8408317+samgabrail@users.noreply.github.com>
2023-06-20 21:31:32 +0000

    update cluster version variable

--- main.tf
+++ main.tf
@@ -41,7 +41,7 @@ module "eks" {
   version = "19.5.1"
 
   cluster_name    = var.cluster_name
-  cluster_version = "1.24"
+  cluster_version = var.cluster_version
 
   vpc_id                         = module.vpc.vpc_id
   subnet_ids                     = module.vpc.private_subnets
