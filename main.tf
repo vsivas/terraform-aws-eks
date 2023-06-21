5ca091b99e77281fd000092eede9aa04e467fba7
Sam <8408317+samgabrail@users.noreply.github.com>
2023-06-21 15:41:13 +0000

    fixed csi add-on version

--- main.tf
+++ main.tf
@@ -85,7 +85,7 @@ module "irsa-ebs-csi" {
 resource "aws_eks_addon" "ebs-csi" {
   cluster_name             = module.eks.cluster_name
   addon_name               = "aws-ebs-csi-driver"
-  addon_version            = "v1.5.2-eksbuild.1"
+  addon_version            = "v1.19.0-eksbuild.2"
   service_account_role_arn = module.irsa-ebs-csi.iam_role_arn
   tags = {
     "eks_addon" = "ebs-csi"
