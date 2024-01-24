0bb011cd1a8f2a3d2c2b02456916ddc7e5cccf16
Siva Sudarsan <vsivas.naidu@gmail.com>
2024-01-24 22:19:35 +0530

    eks addon-ebsCSI modified

--- main.tf
+++ main.tf
@@ -86,7 +86,7 @@ resource "aws_eks_addon" "ebs-csi" {
   cluster_name             = module.eks.cluster_name
   addon_name               = "aws-ebs-csi-driver"
   addon_version            = "v1.19.0-eksbuild.2"
-  service_account_role_arn = module.irsa-ebs-csi.iam_role_arn
+  service_account_role_arn = module.irsa-ebs-csi.iam_role.arn
   tags = {
     "eks_addon" = "ebs-csi"
     "terraform" = "true"
